a255cc9ed2e35949d0fc18cf0bd0fc2c58734f25
Stephen Elliott <stephen.elliott231@gmail.com>
2024-10-12 17:44:11 +1100

    Tests green

--- tests/unit/test_tensorops.cpp
+++ tests/unit/test_tensorops.cpp
@@ -19,11 +19,11 @@
 TEST(TestTensorOps, ZeroTensor)
 {    
     double test_values[3][2] = {
-        {0, 0},
-        {0, 0},
-        {0, 0}
+        {0.0, 0.0},
+        {0.0, 0.0},
+        {0.0, 0.0}
     };
-    torch::Tensor test = torch::from_blob(test_values, {2,3}, {3,3}, {torch::dtype(torch::kFloat64)});
+    torch::Tensor test = torch::from_blob(test_values, {3,2}, {torch::dtype(torch::kFloat64)});
 
     EXPECT_TRUE
     (
@@ -37,9 +37,9 @@ TEST(TestTensorOps, ZeroTensor)
 TEST(TestTensorOps, IdentityTensor)
 {
     double test_values[3][3] = {
-        {1, 0, 0},
-        {0, 1, 0},
-        {0, 0, 1}
+        {1.0, 0.0, 0.0},
+        {0.0, 1.0, 0.0},
+        {0.0, 0.0, 1.0}
     };
     torch::Tensor test = torch::from_blob(test_values, {3,3}, {torch::dtype(torch::kFloat64)});
 
@@ -55,23 +55,23 @@ TEST(TestTensorOps, IdentityTensor)
 TEST(TestTensorOps, TensorSum)
 {
     double test_values_a[3][3] = {
-        {1, 0, 0},
-        {0, 1, -1},
-        {0, 0, 1}
+        {1.0, 0.0, 0.0},
+        {0.0, 1.0, -1.0},
+        {0.0, 0.0, 1.0}
     };
     torch::Tensor ta = torch::from_blob(test_values_a, {3,3}, {torch::dtype(torch::kFloat64)});
 
     double test_values_b[3][3] = {
-        {0, 1, 0},
-        {0, 0.5, 2.1},
-        {7, 0, -5.6}
+        {0.0, 1.0, 0.0},
+        {0.0, 0.5, 2.1},
+        {7.0, 0.0, -5.6}
     };
     torch::Tensor tb = torch::from_blob(test_values_b, {3,3}, {torch::dtype(torch::kFloat64)});
 
     double test_values_c[3][3] = {
-        {1, 1, 0},
-        {0, 1.5, 1.1},
-        {7, 0, -4.6}
+        {1.0, 1.0, 0.0},
+        {0.0, 1.5, 1.1},
+        {7.0, 0.0, -4.6}
     };
     torch::Tensor tc = torch::from_blob(test_values_c, {3,3}, {torch::dtype(torch::kFloat64)});
 
@@ -87,28 +87,28 @@ TEST(TestTensorOps, TensorSum)
 TEST(TestTensorOps, TensorMultiple)
 {
     double test_values_a[3][3] = {
-        {1, 1, 1},
-        {-1, -1, -1},
-        {-2, 2, 0}
+        {1.0, 1.0, 1.0},
+        {-1.0, -1.0, -1.0},
+        {-2.0, 2.0, 0.0}
     };
     torch::Tensor ta = torch::from_blob(test_values_a, {3,3}, {torch::dtype(torch::kFloat64)});
 
     double test_values_b[3][3] = {
-        {-2, -2, -2},
-        {2, 2, 2},
-        {4, -4, 0}
+        {-2.0, -2.0, -2.0},
+        {2.0, 2.0, 2.0},
+        {4.0, -4.0, 0.0}
     };
     torch::Tensor tb = torch::from_blob(test_values_b, {3,3}, {torch::dtype(torch::kFloat64)});
 
     double test_values_c[3][3] = {
         {4.5, 4.5, 4.5},
         {-4.5, -4.5, -4.5},
-        {-9, 9, 0}
+        {-9.0, 9.0, 0.0}
     };
-    torch::Tensor tc = torch::from_blob(test_values_b, {3,3}, {torch::dtype(torch::kFloat64)});
+    torch::Tensor tc = torch::from_blob(test_values_c, {3,3}, {torch::dtype(torch::kFloat64)});
 
     EXPECT_TRUE(
-        tensorops::multiple(ta, -2).equal(tb)
+        tensorops::multiple(ta, -2.0).equal(tb)
     );
 
     EXPECT_TRUE(
@@ -120,97 +120,68 @@ TEST(TestTensorOps, TensorMultiple)
 // Tensor scalar/dot product
 //
 TEST(TestTensorOps, TensorScalarProduct) {
-    double test_values_a[3][3] = {
-        {1, 1, 1},
-        {-1, -1, -1},
-        {-2, 2, 0}
-    };
-    torch::Tensor ta = torch::from_blob(test_values_a, {3,3}, {torch::dtype(torch::kFloat64)});
+    double test_values_a[3] = {1.0, 1.0, 1.0};
+    torch::Tensor ta = torch::from_blob(test_values_a, {3}, {torch::dtype(torch::kFloat64)});
 
-    double test_values_b[3][3] = {
-        {-2, -2, -2},
-        {2, 2, 2},
-        {4, -4, 0}
-    };
-    torch::Tensor tb = torch::from_blob(test_values_b, {3,3}, {torch::dtype(torch::kFloat64)});
+    double test_values_b[3] = {-2.0, 2.0, 4.0};
+    torch::Tensor tb = torch::from_blob(test_values_b, {3}, {torch::dtype(torch::kFloat64)});
 
-    double test_values_aa[3][3] = {
-        {-2, -2, 0},
-        {2, 2, 0},
-        {0, 0, 0}
-    };
-    torch::Tensor taa = torch::from_blob(test_values_a, {3,3}, {torch::dtype(torch::kFloat64)});
-
-    double test_values_ab[3][3] = {
-        {4, -4, 0},
-        {-4, 4, 0},
-        {8, 8, 8}
-    };
-    torch::Tensor tab = torch::from_blob(test_values_a, {3,3}, {torch::dtype(torch::kFloat64)});
-
+    torch::Tensor test_value_ab_result = torch::tensor(4.0, torch::dtype(torch::kFloat64));
+    
     EXPECT_TRUE(
-        tensorops::scalar_product(ta, ta).equal(taa) 
+        tensorops::scalar_product(ta, tb).equal(test_value_ab_result)
     );
 
-    EXPECT_TRUE(
-        tensorops::scalar_product(ta, tb).equal(tab)
-    );
 }
 
 //
 // Tensor product
 //
 TEST(TestTensorOps, TensorProduct) {
-    double test_values_a[2][2] = {
-        {1, 0},
-        {0, 1}
-    };
-    torch::Tensor ta = torch::from_blob(test_values_a, {2,2}, {torch::dtype(torch::kFloat64)});
-
     double test_values_b[2][2] = {
-        {0, 1},
-        {1, 0}
+        {0.0, 1.0},
+        {1.0, 0.0}
     };
     torch::Tensor tb = torch::from_blob(test_values_b, {2,2}, {torch::dtype(torch::kFloat64)});
 
     double test_values_c[2][2] = {
-        {1, 1},
-        {0, 0}
+        {1.0, 1.0},
+        {0.0, 0.0}
     };
     torch::Tensor tc = torch::from_blob(test_values_c, {2,2}, {torch::dtype(torch::kFloat64)});
 
     double test_values_d[2][2] = {
-        {1, 2},
-        {3, 4}
+        {1.0, 2.0},
+        {3.0, 4.0}
     };
     torch::Tensor td = torch::from_blob(test_values_d, {2,2}, {torch::dtype(torch::kFloat64)});
     
     double test_values_dd[2][2] = {
-        {1, 6},
-        {6, 16}
+        {7.0, 10.0},
+        {15.0, 22.0}
     };
     torch::Tensor tdd = torch::from_blob(test_values_dd, {2,2}, {torch::dtype(torch::kFloat64)});
     
     double test_values_identity[2][2] = {
-        {1, 0},
-        {0, 1}
+        {1.0, 0.0},
+        {0.0, 1.0}
     };
     torch::Tensor ti = torch::from_blob(test_values_identity, {2,2}, {torch::dtype(torch::kFloat64)});
 
     double test_values_zeroes[2][2] = {
-        {0, 0},
-        {0, 0}
+        {0.0, 0.0},
+        {0.0, 0.0}
     };
     torch::Tensor tz = torch::from_blob(test_values_zeroes, {2,2}, {torch::dtype(torch::kFloat64)});
 
-    // ta x tb
+    // ti x tb = tb
     EXPECT_TRUE (
-        tensorops::tensor_product(ta, tb).equal(tz)
+        tensorops::tensor_product(ti, tb).equal(tb)
     );
 
-    // ta x tc neq tc x ta
-    torch::Tensor sca = tensorops::tensor_product(tc, ta);
-    torch::Tensor sac = tensorops::tensor_product(ta, tc);
+    // tc x tb =/= tb x tc
+    torch::Tensor sca = tensorops::tensor_product(tc, tb);
+    torch::Tensor sac = tensorops::tensor_product(tb, tc);
 
     EXPECT_FALSE (
         sca.equal(sac)
@@ -223,12 +194,7 @@ TEST(TestTensorOps, TensorProduct) {
 
     // ta x tz
     EXPECT_TRUE (
-        tensorops::tensor_product(ta, tz).equal(tz)
-    );
-
-    // ta x ti
-    EXPECT_TRUE (
-        tensorops::tensor_product(ta, ti).equal(ta)
+        tensorops::tensor_product(ti, tz).equal(tz)
     );
 }
 
@@ -237,8 +203,8 @@ TEST(TestTensorOps, TensorProduct) {
 //
 TEST(TestTensorOps, ReLu) {
     double test_values_a[5][1] = {
-        {-5},
-        {3},
+        {-5.0},
+        {3.0},
         {2.556},
         {-6000.07},
         {405.5}
@@ -246,10 +212,10 @@ TEST(TestTensorOps, ReLu) {
     torch::Tensor ta = torch::from_blob(test_values_a, {5,1}, {torch::dtype(torch::kFloat64)});
 
     double test_values_a_relu[5][1] = {
-        {0},
-        {3},
+        {0.0},
+        {3.0},
         {2.556},
-        {0},
+        {0.0},
         {405.5}
     };
     torch::Tensor ta_relu = torch::from_blob(test_values_a_relu, {5,1}, {torch::dtype(torch::kFloat64)});
@@ -271,14 +237,17 @@ TEST(TestTensorOps, Softmax) {
     torch::Tensor ta = torch::from_blob(test_values_a, {3,1}, {torch::dtype(torch::kFloat64)});
 
     double test_values_a_softmax[3][1] = {
-        {1.3},  // PLACEHOLDER
-        {5.1},
-        {2.2}
+        {0.0208},
+        {0.9282},
+        {0.0511}
     };
     torch::Tensor ta_softmax = torch::from_blob(test_values_a_softmax, {3,1}, {torch::dtype(torch::kFloat64)});
 
+    torch::Tensor error = tensorops::softmax(ta) - ta_softmax;
+    torch::Tensor error_norm = torch::norm(error);
+
     EXPECT_TRUE(
-        tensorops::softmax(ta).equal(ta_softmax)
+        error_norm.less_equal(1e-4).item().to<bool>()
     );
 }
 
@@ -286,16 +255,16 @@ TEST(TestTensorOps, Softmax) {
 // Pipe << operator overload
 //
 TEST(TestTensorOps, PipeOperator) {
-    double test_values[2][2] = {
-        {1, 2},
-        {3, 4}
+    double test_values[2][2] = { // Cpp native double, Torch kfloat64, Torch double are all the same https://pytorch.org/docs/stable/tensors.html
+        {1.0, 2.0},
+        {3.0, 4.0}
     };
     torch::Tensor test_tensor = torch::from_blob(test_values, {2,2}, {torch::dtype(torch::kFloat64)});
 
     std::ostringstream oss;
     oss << test_tensor;
 
-    std::string expected_output = " 1  2\n 3  4\n[ CPUFloatType{2,2} ]";
+    std::string expected_output = " 1  2\n 3  4\n[ CPUDoubleType{2,2} ]";
     EXPECT_EQ(
         oss.str(), expected_output
     );
