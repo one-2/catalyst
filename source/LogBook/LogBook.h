52eb3ac4661905d8ecccc73fba738c6dd148f1b1
Stephen Elliott <stephen.elliott231@gmail.com>
2024-10-30 16:53:29 +1100

    Logging classes refactored

--- source/LogBook/LogBook.h
+++ source/LogBook/LogBook.h
@@ -61,3 +61,17 @@ private:
 };
 
 #endif // LOGBOOK_H
+
+- storage_directory: std::string
+- system_logs: std::list<std::string>
+- model_checkpoints: std::list<std::string>
+- debug_logs: std::list<std::string>
+- Info: struct {key: std::string, string_value: std::string, float_value float}
+- LogEntry: struct{timestamp: std::chrono::time_point<std::chrono::system_clock>, type: std::String, info: std::list<LogBook::Info>}
++ LogBook(storage_directory_path: std::string&)
++ log_info(type: std::string&, info: std::list<Info>): std::string
++ const read_log_type(type: std::string&): std::list<LogEntry>
++ log_checkpoint(model: Model): std::string
+- serialise_log(log: LogEntry&): std::string
+- deserialise_log(std::string&): log: LogEntry
++ std::ostream& operator<<
