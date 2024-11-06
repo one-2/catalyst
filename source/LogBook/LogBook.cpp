dd6a1532eef56f591eaf3a13a9f2b216cc2a02f4
Stephen Elliott <stephen.elliott231@gmail.com>
2024-11-06 16:23:06 +1100

    Document LogBook and change filename convention for log writing to storage

--- source/LogBook/LogBook.cpp
+++ source/LogBook/LogBook.cpp
@@ -10,46 +10,112 @@
 
 namespace logging {
 
+/**
+ * @brief Constructs a new LogBook object.
+ * 
+ * @param storage_directory The directory where log files will be stored.
+ */
 LogBook::LogBook(std::string& storage_directory) 
     : storage_directory(storage_directory) {}
+
+
+
+
 
 LogBook::LogBook(std::string& serialized_directory_root, LogsMap logs_map) 
     : storage_directory(serialized_directory_root), logs_map(logs_map) {}
 
+/**
+ * @brief Logs a system event with the specified epoch and cycle.
+ *
+ * This function creates a SystemLogEntry with the given epoch and cycle,
+ * and then logs it asynchronously.
+ *
+ * @param epoch The epoch time of the system event.
+ * @param cycle The cycle count of the system event.
+ */
 void LogBook::log_system(int epoch, int cycle)
 {
     SystemLogEntry e(epoch, cycle);
     log_async(e);
 }
 
+/**
+ * @brief Logs a checkpoint asynchronously.
+ *
+ * This function creates a CheckpointLogEntry with the provided epoch, cycle, serial, and name,
+ * and then logs it asynchronously.
+ *
+ * @param epoch The epoch number of the checkpoint.
+ * @param cycle The cycle number of the checkpoint.
+ * @param serial The serial number associated with the checkpoint.
+ * @param name The name associated with the checkpoint.
+ */
 void LogBook::log_checkpoint(int epoch, int cycle, std::string& serial, std::string& name)
 {
     CheckpointLogEntry e(epoch, cycle, serial, name);
     log_async(e);
 }
 
+/**
+ * @brief Logs an evaluation entry asynchronously.
+ *
+ * This function creates an EvaluationLogEntry object with the provided epoch,
+ * cycle, score name, and score value, and then logs it asynchronously.
+ *
+ * @param epoch The current epoch number.
+ * @param cycle The current cycle number.
+ * @param score_name The name of the score being logged.
+ * @param score_value The value of the score being logged.
+ */
 void LogBook::log_evaluation(int epoch, int cycle, std::string& score_name, float score_value)
 {
     EvaluationLogEntry e(epoch, cycle, score_name, score_value);
     log_async(e);
 }
 
+/**
+ * @brief Logs a debug message asynchronously.
+ * 
+ * This function creates a DebugLogEntry with the provided epoch, cycle, and message,
+ * and then logs it asynchronously.
+ * 
+ * @param epoch The epoch number associated with the debug message.
+ * @param cycle The cycle number associated with the debug message.
+ * @param message The debug message to be logged.
+ */
 void LogBook::log_debug(int epoch, int cycle, std::string& message)
 {
     DebugLogEntry e(epoch, cycle, message);
     log_async(e);
 }
 
+/**
+ * @brief Logs a log entry asynchronously.
+ *
+ * This function creates a new asynchronous task to log the provided log entry.
+ * The task will add the log entry to the internal map and then write it to storage.
+ *
+ * @param log_entry The log entry to be logged.
+ */
 void LogBook::log_async(const LogEntry& log_entry)
 {
     auto log_task = std::async(std::launch::async, [this, log_entry]() {
         add_log_to_map(log_entry);
         write_log_to_storage(log_entry);
     });
-
-    // TODO: implement local write as well
 }
 
+/**
+ * @brief Adds a log entry to the log map.
+ *
+ * This function adds a given log entry to the log map. If the log type does not
+ * already exist in the map, it creates a new list for that log type and then
+ * adds the log entry to the list. The function uses a mutex to ensure thread
+ * safety when accessing and modifying the log map.
+ *
+ * @param log The log entry to be added to the log map.
+ */
 void LogBook::add_log_to_map(const LogEntry& log)
 {
     // Put a mutex on the log map
@@ -62,6 +128,15 @@ void LogBook::add_log_to_map(const LogEntry& log)
     logs_map[log.get_type()].push_back(log);
 }
 
+/**
+ * @brief Writes a log entry to storage.
+ *
+ * This function serializes a given log entry to JSON format and writes it to a specified
+ * log path in the storage.
+ *
+ * @param log The log entry to be written to storage.
+ * @return A string representing the result of the write operation.
+ */
 std::string LogBook::write_log_to_storage(const LogEntry& log)
 {
     std::string serialized_log = log.to_json();
@@ -69,15 +144,35 @@ std::string LogBook::write_log_to_storage(const LogEntry& log)
     return io::write_log(serialized_log, log_path);
 }
 
+/**
+ * @brief Generates a log file path based on the log entry details.
+ *
+ * This function constructs a file path for a log entry using its epoch, cycle,
+ * and type. The timestamp is appended to prevent overwriting.
+ *
+ * @param log The log entry for which the path is being generated.
+ * @return A string representing the generated log file path.
+ */
 std::string LogBook::generate_log_path(const LogEntry& log)
 {
     // Generate the path
     auto epoch = log.get_epoch();
     auto cycle = log.get_cycle();
-    int random_number = rand() % 1000;
-    return storage_directory + "/" + log.get_type() + "/e" + std::to_string(epoch) + "c" + std::to_string(cycle) + "-" + std::to_string(random_number) + ".log"; // Random number helps prevent overwriting
+    int timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    return storage_directory + "/" + log.get_type() + "/e" + std::to_string(epoch) + "c" + std::to_string(cycle) + "-" + std::to_string(timestamp) + ".log";
 }
 
+/**
+ * @brief Reads logs of a specified type from the log book.
+ *
+ * This function searches for logs of the specified type in the in-memory log map.
+ * If logs of the specified type are found, they are returned as a list.
+ * If no logs of the specified type are found in memory, a range_error exception is thrown.
+ *
+ * @param type The type of logs to read.
+ * @return A list of LogEntry objects of the specified type.
+ * @throws std::range_error If no logs of the specified type are found in memory.
+ */
 const std::list<LogEntry> LogBook::read_logs(const std::string& type) const
 {
     std::list<LogEntry> filtered_logs;
--- tests/unit/test_logbook.cpp
+++ tests/unit/test_logbook.cpp
@@ -40,6 +40,17 @@ protected:
 };
 
 
+/**
+ * @brief Test case for reading system logs from the LogBook.
+ *
+ * This test case verifies that the LogBook correctly logs a system event
+ * and that the logs can be read back accurately. It performs the following steps:
+ * 1. Logs a system event with epoch 1 and cycle 1.
+ * 2. Reads the logs of type "system".
+ * 3. Asserts that the size of the logs is 1.
+ * 4. Asserts that the epoch of the first log entry is 1.
+ * 5. Asserts that the cycle of the first log entry is 1.
+ */
 TEST_F(LogBookTest, ReadSystemLogs) {
     logBook->log_system(1, 1);
     auto logs = logBook->read_logs("system");
@@ -48,6 +59,17 @@ TEST_F(LogBookTest, ReadSystemLogs) {
     ASSERT_EQ(logs.front().get_cycle(), 1);
 }
 
+/**
+ * @brief Unit test for reading checkpoint logs from the LogBook.
+ *
+ * This test case verifies that a checkpoint log entry can be successfully
+ * logged and read from the LogBook. It performs the following steps:
+ * 1. Logs a checkpoint with specific epoch, cycle, serial, and name.
+ * 2. Reads the logs filtered by "checkpoint".
+ * 3. Asserts that the size of the logs is 1.
+ * 4. Asserts that the epoch of the first log entry is 1.
+ * 5. Asserts that the cycle of the first log entry is 1.
+ */
 TEST_F(LogBookTest, ReadCheckpointLogs) {
     std::string serial = "serial";
     std::string name = "name";
@@ -58,6 +80,19 @@ TEST_F(LogBookTest, ReadCheckpointLogs) {
     ASSERT_EQ(logs.front().get_cycle(), 1);
 }
 
+/**
+ * @brief Test case for reading evaluation logs from the LogBook.
+ *
+ * This test case logs an evaluation with specific parameters and then reads
+ * the logs to verify that the log entry was correctly recorded.
+ *
+ * @details
+ * - Logs an evaluation with epoch 1, cycle 1, score name "accuracy", and score value 0.6.
+ * - Reads the logs of type "evaluation".
+ * - Asserts that the size of the logs is 1.
+ * - Asserts that the epoch of the first log entry is 1.
+ * - Asserts that the cycle of the first log entry is 1.
+ */
 TEST_F(LogBookTest, ReadEvaluationLogs) {
     std::string score_name = "accuracy";
     logBook->log_evaluation(1, 1, score_name, 0.6);
@@ -67,6 +102,16 @@ TEST_F(LogBookTest, ReadEvaluationLogs) {
     ASSERT_EQ(logs.front().get_cycle(), 1);
 }
 
+/**
+ * @brief Unit test for reading debug logs from the LogBook.
+ *
+ * This test case verifies that a debug log entry can be successfully written
+ * and read from the LogBook. It performs the following steps:
+ * 1. Logs a debug message with specific epoch and cycle values.
+ * 2. Reads the logs filtered by the "debug" log level.
+ * 3. Asserts that the size of the retrieved logs is 1.
+ * 4. Asserts that the epoch and cycle values of the retrieved log match the expected values.
+ */
 TEST_F(LogBookTest, ReadDebugLogs) {
     std::string message = "Debug message";
     logBook->log_debug(1, 1, message);
@@ -76,10 +121,31 @@ TEST_F(LogBookTest, ReadDebugLogs) {
     ASSERT_EQ(logs.front().get_cycle(), 1);
 }
 
+/**
+ * @brief Test case to verify the behavior of reading non-existent logs.
+ *
+ * This test case checks if the LogBook class throws a std::range_error
+ * exception when attempting to read logs that do not exist.
+ *
+ * @note This test uses the Google Test framework.
+ */
 TEST_F(LogBookTest, ReadNonExistentLogs) {
     ASSERT_THROW(logBook->read_logs("nonexistent"), std::range_error);
 }
 
+/**
+ * @brief Unit test for the LogBook class to verify the log_system function.
+ *
+ * This test case checks if the log_system function correctly logs the system
+ * state by verifying the size of the logs and the values of epoch and cycle.
+ *
+ * @details
+ * - Calls log_system with the current epoch and cycle from the model.
+ * - Reads the logs with the type "system".
+ * - Asserts that the size of the logs is 1.
+ * - Asserts that the epoch of the first log is 1.
+ * - Asserts that the cycle of the first log is 1.
+ */
 TEST_F(LogBookTest, TestWriteLogSystem) {
     logBook->log_system(model->get_epoch(), model->get_cycle());
     auto logs = logBook->read_logs("system");
@@ -89,6 +155,18 @@ TEST_F(LogBookTest, TestWriteLogSystem) {
 
 }
 
+/**
+ * @brief Unit test for the LogBook class to verify the functionality of writing a log checkpoint.
+ *
+ * This test case checks if a checkpoint log entry is correctly written to the log book.
+ * It performs the following steps:
+ * 1. Creates a serial and name string.
+ * 2. Calls the log_checkpoint method of the logBook object with the current epoch and cycle from the model, along with the serial and name.
+ * 3. Reads the logs with the type "checkpoint".
+ * 4. Asserts that the size of the logs is 1.
+ * 5. Asserts that the epoch of the first log entry is 1.
+ * 6. Asserts that the cycle of the first log entry is 1.
+ */
 TEST_F(LogBookTest, TestWriteLogCheckpoint) {
     std::string serial = "serial";
     std::string name = "name";
@@ -100,6 +178,18 @@ TEST_F(LogBookTest, TestWriteLogCheckpoint) {
 
 }
 
+/**
+ * @brief Unit test for the LogBook class to verify the log_evaluation method.
+ *
+ * This test checks if the log_evaluation method correctly logs an evaluation
+ * with the given parameters and if the log can be read back correctly.
+ *
+ * @details
+ * - Sets up a score name "accuracy" and logs an evaluation with epoch 1, cycle 1, and score 0.6.
+ * - Reads back the logs and verifies that there is exactly one log entry.
+ * - Checks that the epoch and cycle of the log entry are correct.
+ * - Verifies that the logged data contains the correct score name and value.
+ */
 TEST_F(LogBookTest, TestWriteLogEvaluation) {
     std::string score_name = "accuracy";
     logBook->log_evaluation(
@@ -122,6 +212,17 @@ TEST_F(LogBookTest, TestWriteLogEvaluation) {
 
 }
 
+/**
+ * @brief Unit test for the LogBook class to verify the functionality of writing a debug log.
+ *
+ * This test case checks if a debug message is correctly logged and can be retrieved.
+ * 
+ * - It logs a debug message with the current epoch and cycle.
+ * - It reads the logs filtered by the "debug" level.
+ * - It asserts that there is exactly one log entry.
+ * - It asserts that the epoch and cycle of the log entry are correct.
+ * - It asserts that the log entry contains the expected message.
+ */
 TEST_F(LogBookTest, TestWriteLogDebug) {
     std::string message = "Debug message";
     logBook->log_debug(model->get_epoch(), model->get_cycle(), message);
@@ -141,6 +242,20 @@ TEST_F(LogBookTest, TestWriteLogDebug) {
 }
 
 
+/**
+ * @brief Unit test for writing all types of log entries to storage.
+ * 
+ * This test verifies that the `write_log` function of the `MockIO` class correctly writes
+ * different types of log entries (`SystemLogEntry`, `CheckpointLogEntry`, `EvaluationLogEntry`, 
+ * and `DebugLogEntry`) to their respective expected file paths and ensures that the paths 
+ * returned by the `write_log` function match the expected paths.
+ * 
+ * The test performs the following steps:
+ * 1. Creates a `SystemLogEntry` and writes it to the expected path, then checks if the returned path matches the expected path.
+ * 2. Creates a `CheckpointLogEntry` with a serial and name, writes it to the expected path, then checks if the returned path matches the expected path.
+ * 3. Creates an `EvaluationLogEntry` with a score name and value, writes it to the expected path, then checks if the returned path matches the expected path.
+ * 4. Creates a `DebugLogEntry` with a message, writes it to the expected path, then checks if the returned path matches the expected path.
+ */
 TEST_F(LogBookTest, WriteAllLogsToStorage) {
     // SystemLogEntry
     logging::SystemLogEntry system_log_entry(1, 1);
