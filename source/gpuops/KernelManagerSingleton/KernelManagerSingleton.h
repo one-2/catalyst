87cb08b77f9ce80a087b8a21976410fd22a3d264
Stephen Elliott <stephen.elliott231@gmail.com>
2024-11-10 03:20:58 +1100

    Create directory structure

--- source/gpuops/KernelManagerSingleton/KernelManagerSingleton.h
+++ source/gpuops/KernelManagerSingleton/KernelManagerSingleton.h
@@ -1,16 +1,17 @@
-// KernelCacheSingleton/KernelCacheSingleton.h
+// KernelManagerSingleton/KernelManagerSingleton.h
 //
 // Author: Stephen Elliott
 //
 // Date: 2024-11-05
 //
-// Description: Singleton class to store a cache of compiled kernels.
+// Description: Singleton class to store a cache of compiled kernels and an access interface.
+//              Async-extensible.
 //
 
 namespace gpuops
 {
 
-class KernelCacheSingleton
+class KernelManagerSingleton
 {
 public:
     KernelCallback get_kernel(std::string type, Args args); // < Replace args struct with generalisation
@@ -18,7 +19,7 @@ public:
 
 private:
     // Method:
-    KernelCallback search_cache(std::string type, Args args); // TODO: Throw error if not found
+    KernelCallback search_Manager(std::string type, Args args); // TODO: Throw error if not found
     KernelCallback build_kernel(std::string type, Args args);
 
     // TODO attribute: Some Map of compiled kernel (shared?) pointers? What
