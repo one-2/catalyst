b1c4ff785d12c7344b52b78dfe942071ece0f55f
Stephen Elliott <stephen.elliott231@gmail.com>
2024-11-12 16:24:28 +1100

    Draft Relu and Matmul kernel headers

---
+++ source/gpuops/Kernel/MatmulKernel/MatmulKernel.h
@@ -0,0 +1,15 @@
+
+
+#ifndef MATMULKERNEL_H
+#define MATMULKERNEL_H
+
+
+class MatmulKernel : public Kernel
+{
+public:
+
+private:
+
+}
+
+#endif MATMULKERNEL_H
---
+++ source/gpuops/Kernel/ReluKernel/ReluKernel.h
@@ -0,0 +1,13 @@
+#ifndef RELUKERNEL_H
+#define RELUKERNEL_H
+
+
+class ReLuKernel : public Kernel
+{
+public:
+
+private:
+
+}
+
+#endif RELUKERNEL_H
