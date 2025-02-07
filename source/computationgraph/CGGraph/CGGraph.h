6c01bd4d605e4f980228ad0d5a1974429f6d1f61
Stephen Elliott <stephen.elliott231@gmail.com>
2025-02-07 14:00:42 +1100

    Simplify CG headers

--- source/computationgraph/CGGraph/CGGraph.h
+++ source/computationgraph/CGGraph/CGGraph.h
@@ -17,6 +17,7 @@
 #include <list>
 #include <functional>
 #include "TensorOps.h"
+#include "CGNode.h"
 
 namespace computationgraph
 {
@@ -27,22 +28,32 @@ public:
     CGGraph();
 
     // Setters
-    void set_optimiser(std::function<void()> optimiser);
     void set_loss(std::function<void()> loss);
 
     // Graph operations
     void add_layer(TensorOps operation, std::function<void()> activation); // TODO change operation to a callback
-    void add_edge(int from, int to);                                       //      how to do autodiff on these?
+    void add_neural_layer(int depth, int width);                                       //      how to do autodiff on these?
                                                                            //      probs need to write the callbacks
                                                                            //      to some interface
-    std::vector<int> topo_sort();
-    void compile();
-    static CGGraph merge(const std::list<CGGraph>& graphs);
 
+    // Graph execution
+    void forward(DataLoader& dataloader);
+    void backward(DataLoader& dataloader);
+
+    
 private:
-    std::vector<std::vector<int>> graph_adj_list;
-    std::function<void()> optimiser_;
-    int validate_layer();
+    // Graph data
+    std::vector<std::vector<int>> graph_adj_list_;
+    std::vector<float> last_loss_;
+
+    // Graph operations
+    void add_layer_(TensorOps operation, std::function<void()> activation); // TODO change operation to a callback
+    int validate_layer_(); // Checks validity of a newly added layer
+
+    // Graph execution
+    std::vector<CGNode> topo_sort_();
+    void optimise_();
+
 };
 
 } // namespace computationgraph
--- source/computationgraph/CGNode/CGNode.h
+++ source/computationgraph/CGNode/CGNode.h
@@ -23,31 +23,23 @@ public:
     // Constructors
     CGNode(int batch_size);
 
-    // Setters
-    void set_activation_function(std::function<TensorPtr(TensorPtr)> func);
-    void set_gradient_function(std::function<TensorPtr(TensorPtr)> func);
+    // Computation
+    SharedTensorPtr compute_activation(SharedTensorPtr inputs);
+    SharedTensorPtr compute_gradient(SharedTensorPtr inputs);
     void update_weight(float learning_rate);
-
-    // Computations
-    TensorPtr compute_activation(TensorPtr input);
-    TensorPtr compute_gradient(TensorPtr input);
 
 
 private:
     // Callbacks
-    std::function<void()> input_function_; // TODO parameterise properly
-    std::function<TensorPtr(TensorPtr)> activation_function_;
-    std::function<TensorPtr(TensorPtr)> gradient_function_; // TODO which autodiff package to use?
+    TensorOps& input_function_;
+    TensorOps& activation_function_;
+    TensorOps& gradient_function_;
 
     // Data
-    TensorPtr input_;
-    TensorPtr activation_;
-    TensorPtr gradient_;
-    TensorPtr weight_;
-
-    // Hyperparameters
-    int batch_size_;
-
+    SharedTensorPtr activations_;
+    float mean_activation_;
+    SharedTensorPtr gradients_;
+    SharedTensorPtr weight_;
 };
 
 } // namespace computationgraph
